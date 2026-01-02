8add48e8a61e854241720feb95b9bbd3c8368e9f
yudo1023 <yudo1023@gmail.com>
2026-01-02 17:21:56 +0900

    feat: 티켓 서비스 모듈을 명명된 가져오기 방식으로 변경

--- src/pages/ReservationPage.jsx
+++ src/pages/ReservationPage.jsx
@@ -7,7 +7,7 @@ import Card from "@components/Card";
 import movieInfo from "@data/movieInfo";
 import messages from "@data/messages";
 
-import ticketService from "@api/TicketService";
+import { ticketService } from "@api/TicketService";
 
 export default function ReservationPage() {
   const { TCuritySDK } = window;
