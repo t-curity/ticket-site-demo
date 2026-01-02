6931b7e785cc6e6ebe131aaea6404aa462ee2303
yudo1023 <yudo1023@gmail.com>
2026-01-02 17:17:42 +0900

    feat: 티켓 서비스 API 추가

--- src/pages/ReservationPage.jsx
+++ src/pages/ReservationPage.jsx
@@ -7,6 +7,8 @@ import Card from "@components/Card";
 import movieInfo from "@data/movieInfo";
 import messages from "@data/messages";
 
+import ticketService from "@api/TicketService";
+
 export default function ReservationPage() {
   const { TCuritySDK } = window;
 
